06181fc682693738ec9b851832d252531960fd9e
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-01-10 10:37:32 +0900

    8

--- atcoder/180/b.py
+++ atcoder/180/b.py
@@ -1,2 +1,14 @@
-n,a,b=list(map(int,input().split()))
-print(n-a+b)
+import math
+n=int(input())
+abs_list=[]
+yugrid=[]
+in_list=list(map(int,input().split()))
+for i in in_list:
+    abs_list.append(abs(i))
+    yugrid.append(i**2)
+a=sum(abs_list)
+b=sum(yugrid)
+c=max(abs_list)
+print(a)
+print(math.sqrt(b))
+print(c)

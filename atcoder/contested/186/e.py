a38e845763350701fb66a45b23270228b612157b
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-01-31 11:03:10 +0900

    solve e

--- atcoder/contested/186/e.py
+++ atcoder/contested/186/e.py
@@ -6,11 +6,8 @@ for _ in range(t):
     if s % d != 0:
         print(-1)
         continue
-    print(n,s,k)
     n //= d
     s //= d
     k //= d
-    print('ddddd')
-    print(n,s,k)
     k_mod = pow(k,-1,n)
-    print(-k_mod * s)
+    print((-k_mod * s) % n)

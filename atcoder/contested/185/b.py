882aeca5aa7654bd858203956abdb550b6be9b9d
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-01-31 15:15:58 +0900

    2

--- atcoder/contested/185/b.py
+++ atcoder/contested/185/b.py
@@ -1,18 +1,12 @@
 n,m,t=list(map(int,input().split()))
 batteries=n
-roots=[False]*t
-for _ in range(m):
+pre = 0
+for i in range(m):
     a,b=list(map(int,input().split()))
-    a-=1
-    b-=1
-    for i in range(a+1,b+1):
-        roots[i] = True
-for i in range(t):
-    if roots[i]:
-        batteries = min(n,batteries+1)
-    else:
-        batteries -= 1
+    batteries-=a - pre
     if batteries <= 0:
         print("No")
         exit(0)
-print("Yes")
+    pre = b
+    batteries=min(b-a+batteries,n)
+print("Yes" if (t-pre) < batteries else "No")

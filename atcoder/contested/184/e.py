e05c15af08df5ae4fb5945a4ae545e311eec540d
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-02-04 23:06:51 +0900

    tmp

--- atcoder/contested/184/e.py
+++ atcoder/contested/184/e.py
@@ -1,14 +1,16 @@
 import heapq
-
+import sys
+input=sys.stdin.readline
 h,w=list(map(int,input().split()))
-grid=[input().rstrip() for _ in range(h)]
+grid=[]
 h_table={}
-checked={}
 d_h=[1,0,-1,0]
 d_w=[0,1,0,-1]
 for h_i in range(h):
+    ll = input().rstrip()
+    grid.append(ll)
     for w_i in range(w):
-        target=grid[h_i][w_i]
+        target=ll[w_i]
         if target=='S':
             s_h=h_i
             s_w=w_i
@@ -16,35 +18,37 @@ for h_i in range(h):
             e_h=h_i
             e_w=w_i
         elif 'a'<=target<='z':
-            if target in h_table:
+            if target in h_table.keys():
                 h_table[target].append([h_i,w_i])
             else:
                 h_table[target] = [[h_i,w_i]]
-q=[[0,s_h,s_w]]
-passed=[[False] * w for _ in range(h)]
-passed[s_h][s_w]=True
+q=[[s_h,s_w]]
+cnt_table=[[-1] * w for _ in range(h)]
+cnt_table[s_h][s_w]=0
 heapq.heapify(q)
 ans = float('inf')
 while len(q) > 0:
     nx=heapq.heappop(q)
-    cnt,n_h,n_w = nx
+    n_h,n_w = nx
     for i in range(4):
         hi=n_h+d_h[i]
         wi=n_w+d_w[i]
-        if not(0<=hi<=h-1 and 0<=wi<=w-1) or grid[hi][wi] == '#' or passed[hi][wi]:continue
-        passed[hi][wi]=True
+        if not(0<=hi<=h-1 and 0<=wi<=w-1) or grid[hi][wi] == '#' or cnt_table[hi][wi] >= 0:continue
+        cnt_table[hi][wi]=cnt_table[n_h][n_w]+1
         if hi==e_h and wi==e_w:
-            ans=min(ans,cnt+1)
-            break
-        heapq.heappush(q,[cnt+1,hi,wi])
-        if grid[hi][wi] != '.' and not grid[hi][wi] in checked:
-            checked[grid[hi][wi]]=True
+            ans=cnt_table[n_h][n_w]+1
+            print(ans)
+            exit(0)
+        heapq.heappush(q,[hi,wi])
+        if grid[hi][wi] == '.':
+            heapq.heappush(q,[hi,wi])
+            cnt_table[hi][wi] = cnt_table[n_h][n_w]+1
+        if grid[hi][wi] in h_table:
             for ll in h_table[grid[hi][wi]]:
                 x,y=ll
-                if not passed[x][y]:
-                    if 0<=abs(x-hi)<=1 and 0<=abs(y-wi)<=1:
-                        heapq.heappush(q,[cnt+1,x,y])
-                    else:
-                        heapq.heappush(q,[cnt+2,x,y])
-                    passed[x][y]=True
+                if cnt_table[x][y]>=0:continue
+                cntt= cnt_table[n_h][n_w]+1 if (x==hi and abs(y-wi) == 1) or (y==wi and abs(x-hi)) else cnt_table[n_h][n_w]+2
+                heapq.heappush(q,[x,y])
+                cnt_table[x][y]=cntt
+                h_table[grid[hi][wi]]=[]
 print(ans if ans!=float('inf') else -1)

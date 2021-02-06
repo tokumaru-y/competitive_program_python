b875a73c0d5df5b409d3d2a0faa947e74232f59f
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-02-06 23:26:22 +0900

    2

--- atcoder/contested/191/c.py
+++ atcoder/contested/191/c.py
@@ -1,28 +1,15 @@
 H,W=list(map(int,input().split()))
-grid = [input().rstrip() for _ in range(H)]
-eh=-1
-ew=-1
-for h in range(H):
-    for w in range(W):
-        if grid[h][w]=="#":
-            eh=h
-            ew=w
-            break
-    if eh!=-1 and ew !=-1:break
-cnt=[[False] * W for _ in range(H)]
-cnt[eh][ew]=True
-dd = [[1,0],[0,1]]
-total=0
-def dfs(x,y,dirx):
-    global total
-    for d in dd:
-        nh=x+d[0]
-        nw=y+d[1]
-        if not(0<=nh<=H-1 and 0<=nw<=W-1):continue
-        if grid[nh][nw]=='#' and d != dirx:
-            total+=1
-        if grid[nh][nw]=="#" and not cnt[nh][nw]:
-            cnt[nh][nw]=True
-            dfs(x,y,d)
-dfs(eh,ew,[0,0])
-print(total)
+grid=[input().rstrip() for _ in range(H)]
+ans=0
+dirc=[[0,0],[0,1],[1,0],[1,1]]
+for h in range(H-1):
+    for w in range(W-1):
+        tmp=[]
+        for d in dirc:
+            nh=h+d[0]
+            nw=w+d[1]
+            tmp.append(grid[nh][nw])
+        cnt = tmp.count("#")
+        if cnt==1 or cnt==3:
+            ans+=1
+print(ans)

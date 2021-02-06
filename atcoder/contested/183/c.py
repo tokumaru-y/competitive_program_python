dc7e47cc6423451417bf821a450af1e62f4f20ee
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-02-06 13:58:05 +0900

    1

--- atcoder/contested/183/c.py
+++ atcoder/contested/183/c.py
@@ -1,3 +1,15 @@
+import itertools
 n,k=list(map(int,input().split()))
-times=[list(map(int,input().split())) for _ in range(n)]
-dp=[[0] * n for _ in range(n)]
+time_table=[list(map(int,input().split())) for _ in range(n)]
+cities = [i+1 for i in range(n-1)]
+ans=0
+for per_list in itertools.permutations(cities):
+    tmp_sum=0
+    pre = 0
+    for c in per_list:
+        tmp_sum+=time_table[pre][c]
+        pre=c
+    tmp_sum+=time_table[pre][0]
+    if tmp_sum == k:
+        ans+=1
+print(ans)

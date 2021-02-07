532cba30914d177a7249a7ec1051d12cf4c01470
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-02-07 18:09:57 +0900

    tmp

--- atcoder/contested/191/e.py
+++ atcoder/contested/191/e.py
@@ -1,35 +1,26 @@
 import heapq
 N,M=list(map(int,input().split()))
-dp=[[float('inf')] * N for _ in range(N)]
-roots=[{} for _ in range(N)]
-for _ in range(M):
-    a,b,c=list(map(int,input().split()))
-    a-=1
-    b-=1
-    roots[a][b]= c if b not in roots[a] else min(c,roots[a][b])
+ABC=[list(map(int,input().split())) for _ in range(N)]
+edges=[[float('inf')] * N for _ in range(N)]
+ans=[]
+for a,b,c in ABC:
+    a,b=a-1,b-1
+    edges[a][b]=min(edges[a][b],c)
 for i in range(N):
-    q=[[0,i]]
+    q=[]
     heapq.heapify(q)
-    passed=[False]*N
-    passed[i]=True
-    while len(q) >0 :
-        h=heapq.heappop(q)
-        cnt,top=h
-        if len(roots[top]) > 0:
-            for k,v in roots[top].items():
-                if not passed[k]:
-                    passed[k]=True
-                    sum_num=cnt+v
-                    dp[i][k]=sum_num
-                    heapq.heappush(q,[sum_num,top])
-    print(passed,i)
-print(dp)
-print(roots)
-for i in range(N):
-    tmp=float('inf') if dp[i][i] != float('inf') else dp[i][i]
-    for j in range(N):
-        tmp1=dp[i][j]+dp[j][i]
-        tmp=min(tmp,tmp1)
-    ans[i]=min(ans[i],tmp)
-print(*ans,sep='\n')     
-
+    costs=[float('inf')] * N
+    heapq.heappush(q,[0,i])
+    c = float('inf')
+    while len(q) > 0:
+        cos,top=heapq.heappop(q)
+        if costs[top] < cos:continue
+        costs[top]=cos
+        if i==0:print(i,cos,top,edges[i])
+        for j in range(N):
+            tmp = edges[top][j]
+            if costs[j] > tmp + cos :
+                heapq.heappush(q,[tmp+cos,j])
+            if j == i and tmp != float('inf'):
+                c=min(c,tmp + cos)
+    print(c if c != float('inf') else -1)

4667166e3583ae8d9f9102e4e84216517b849638
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-02-07 12:43:40 +0900

    a small  change

--- atcoder/contested/191/d.py
+++ atcoder/contested/191/d.py
@@ -8,7 +8,7 @@ for i in range(-2*10**5,2*10**5+1):
     tmp = r**2 - (x-i)**2
     if tmp >= 0:
         tmp = math.isqrt(tmp)
-        low = -(-(y-tmp)//10000)
+        low = (-(y-tmp)//10000)
         high = (y+tmp)//10000
-        ans += high - low + 1
+        ans += high + low + 1
 print(ans)

fc97960056d0916f747cb886c38b6204ae0798da
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-02-07 14:42:07 +0900

    tmp

--- atcoder/contested/191/e.py
+++ atcoder/contested/191/e.py
@@ -1,32 +1,42 @@
-import sys
-sys.setrecursionlimit(10000)
+import heapq
 N,M=list(map(int,input().split()))
-graph = [[] for _ in range(N)]
-costs=[[float('inf')]*N for _ in range(N)]
-ans=[0]*N
+dp=[[float('inf')] * N for _ in range(N)]
+roots=[{} for _ in range(N)]
 for _ in range(M):
     a,b,c=list(map(int,input().split()))
     a-=1
     b-=1
-    graph[a].append(b)
-    costs[a][b]=min(costs[a][b],c)
-
-def can_return(ll,origin,past,cnt):
-    res = float('inf')
-    if len(ll) > 0:
-        for l in ll:
-            if l==origin:
-                return cnt+costs[past][l]
-                break
-            if passed[l]:continue
-            passed[l]=True
-            tmp = can_return(graph[l],origin,l,cnt+costs[past][l])
-            res = min(res,tmp)
-    return res
+    roots[a][b]= c if b not in roots[a] else min(c,roots[a][b])
 for i in range(N):
-    passed=[False]*N
-    passed[i]=True
-    res = can_return(graph[i],i,i,0)
-    ans[i]=res if res != float("inf") else -1
+    for j in range(N):
+        if i==j:
+            if j in roots[i]:
+                dp[i][j]=min(dp[i][j],roots[i][j])
+            continue
+        q=[]
+        passed = [False]*N
+        passed[i]=True
+        heapq.heapify(q)
+        # [cnt, now_top]
+        heapq.heappush(q,[0,i])
+        tmp = float('inf')
+        while len(q) > 0:
+            cnt,top = heapq.heappop(q)
+            if len(roots[top]) > 0:
+                if tmp==float('inf'):tmp=0
+                for k,v in roots[top].items():
+                    if not passed[k]:
+                        passed[k] = True
+                        heapq.heappush(q,[cnt+v,k])
+                        tmp += cnt+v
+        dp[i][j]=tmp
+ans=[float('inf') for _ in range(N)]
+print(dp)
+for i in range(N):
+    tmp=float('inf') if dp[i][i] != float('inf') else dp[i][i]
+    for j in range(N):
+        tmp1=dp[i][j]+dp[j][i]
+        tmp=min(tmp,tmp1)
+    ans[i]=min(ans[i],tmp)
+print(*ans,sep='\n')     
 
-print(*ans,sep='\n')

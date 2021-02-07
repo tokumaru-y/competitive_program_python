6b4d4ae5f8322a625e6f8f29ee2768f7ef1939e6
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-02-07 18:34:03 +0900

    pre changes

--- atcoder/contested/191/e.py
+++ atcoder/contested/191/e.py
@@ -20,13 +20,15 @@ for i in range(N):
     c = float('inf')
     while len(q) > 0:
         cos,top=heapq.heappop(q)
-        if len(edges[top]) ==0:continue
+        if top not in edges:continue
         for k,v in edges[top].items():
             tmp_cnt=v + cos
             if costs[k] > tmp_cnt:
                 costs[k]=tmp_cnt
                 heapq.heappush(q,[tmp_cnt,k])
-            if i == k and tmp_cnt != float('inf'):
+            if i == k and not top == k and tmp_cnt != float('inf'):
                 c = min(c,tmp_cnt)
-                
+                q=[]
+                break
+    if i in edges[i]:c = min(c,edges[i][i])
     print(c if c != float('inf') else -1)

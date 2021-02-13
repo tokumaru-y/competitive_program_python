1a6d16520cdf845fa76eec7a555a5558ffa6721f
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-02-13 23:32:42 +0900

    fix 3

--- atcoder/189/c.py
+++ atcoder/189/c.py
@@ -3,8 +3,8 @@ A=list(map(int,input().split()))
 ans=0
 for i in range(N):
     min_num=A[i]
-    tmp_sum=A[i]
-    for j in range(i+1,N):
+    tmp_sum=0
+    for j in range(N):
         if min_num > A[j]:
             ans=max(ans,tmp_sum)
             tmp_sum=0

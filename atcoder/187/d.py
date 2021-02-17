173af99aa2892c4c4be697fd43927543aa440787
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-02-17 23:08:31 +0900

    4

--- atcoder/187/d.py
+++ atcoder/187/d.py
@@ -1,12 +1,14 @@
 N=int(input())
 AB=[list(map(int,input().split())) for _ in range(N)]
 X=[]
-sa=0
+s=0
 for a,b in AB:
-    X.append([a,b,a+b])
-    sa+=a
-X.sort(lambda x : -x[2])
-Takahashi=0
+    X.append([a,b,2*a+b])
+    s += -a
+X=sorted(X, key = lambda x : -x[2])
 for i in range(N):
     a,b,c=X[i]
-    
+    s+=c
+    if s > 0:
+        print(i+1)
+        exit()

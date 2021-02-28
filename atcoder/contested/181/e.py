18994b06ddebb518f95f0cd8a1742c3afbf04a13
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-03-01 01:13:03 +0900

    4

--- atcoder/contested/181/e.py
+++ atcoder/contested/181/e.py
@@ -2,30 +2,17 @@ import bisect
 N,M=list(map(int,input().split()))
 H=list(map(int,input().split()))
 W=list(map(int,input().split()))
-pre_list = [0]*(N//2+1)
-back_list = [0]*(N//2+1)
+pre_list = [0]*(N+1)
+back_list = [0]*(N+1)
 H.sort()
-# W.sort()
-for i in range(1,N//2):
-    pre_list[i] = pre_list[i-1] + abs(H[2*i] -H[2*i+1]) if i!=0 else abs(H[0]-H[1])
-    back_list[i] = back_list[i-1] + abs(H[N-1-2*i] - H[N-2-2*i]) if i != 0 else abs(H[N-1] - H[N-2])
+for i in range(2, N, 2):
+    pre_list[i] = pre_list[i-2] + H[i-1] - H[i-2]
+    back_list[i] = back_list[i-2] + H[N-i+1] - H[N-i]
 ans = float('inf')
-# print(pre_list)
-# print(back_list)
-for i in range(M):
-    w = W[i]
+for w in W:
     ind = bisect.bisect_left(H,w)
-    # if ind == N:
-    #     ind-=1
-    # if ind == 0:
-    #     tmp_sum = pre_list[ind//2-1]+back_list[(N//2)-(ind//2)-1]
-    #     tmp_sum+= abs(w-H[ind])
-    #     # print(i, tmp_sum,  ind)
-    # else:
-    ind1,ind2 = ind-1,ind+1
-    comp1 = pre_list[ind1//2-1]+back_list[(N//2)-1-(ind1//2)]+abs(w-H[ind1]) if 0<= ind1 <= N-1 else float('inf')
-    comp2 = pre_list[ind2//2-1]+back_list[(N//2)-1-(ind2//2)]+abs(w-H[ind2]) if 0<= ind2 <= N-1 else float('inf')
-    tmp_sum = min(comp1,comp2)
-    print(i,ind1,ind2,comp1,comp2)
-    ans = min(ans, tmp_sum)
+    if ind%2==0:
+        ans = min(ans, pre_list[ind] + back_list[N-ind-1] + abs(w-H[ind]))
+    else:
+        ans = min(ans, pre_list[ind-1] + back_list[N-ind] + abs(w-H[ind-1]))
 print(ans)

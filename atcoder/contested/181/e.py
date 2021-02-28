7d55a0d16b1285e560a966be04717c305390a547
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-02-28 14:08:05 +0900

    tmp

--- atcoder/contested/181/e.py
+++ atcoder/contested/181/e.py
@@ -1,29 +1,23 @@
+import bisect
 N,M=list(map(int,input().split()))
 H=list(map(int,input().split()))
 W=list(map(int,input().split()))
-sum_list = [0]*((N+1)//2)
+pre_list = [0]*(N//2)
+back_list = [0]*(N//2)
 H.sort()
-W.sort()
-def search(h):
-    left = 0
-    right = M-1
-    while right-left > 1:
-        middle = (left+right)//2
-        if W[middle] >= h:
-            right = middle
-        else:
-            left = middle
-    return left, right
-for i in range((N+1)//2):
-    sum_list[i+1] = H[i*2+1] - H[i*2] + sum_list[i]
+# W.sort()
+for i in range(2,N,2):
+    pre_list[i-2] = pre_list[i-1] + abs(H[i+1] -H[i])
+    back_list[i-2] = back_list[i-1] + abs(H[N-i] - H[N-i-1])
 ans = float('inf')
-for i in range(H):
-    h = H[i]
-    l_index, r_index=search(h)
-    left = W[l_index]
-    right = W[r_index]
-    if abs(h-left) >= abs(h-right):
-        target_index = right if right%2==0 else left
-    else:
-        target_index = left if left%2==0 else right
-    left_sum = 
+print(pre_list)
+print(back_list)
+for i in range(M):
+    w = W[i]
+    ind = bisect.bisect_left(H,w)
+    if ind % 2 ==1:continue
+    print(ind)
+    tmp_sum = pre_list[ind//2-1]+back_list[(N//2)-1-(ind//2)]
+    tmp_sum+= abs(w-H[ind])
+    ans = min(ans, tmp_sum)
+print(ans)

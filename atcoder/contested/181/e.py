6ef4b1d53d7aeef83d8a3fb570e92d0c0465cb30
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-02-28 14:18:26 +0900

    tmp

--- atcoder/contested/181/e.py
+++ atcoder/contested/181/e.py
@@ -6,18 +6,23 @@ pre_list = [0]*(N//2)
 back_list = [0]*(N//2)
 H.sort()
 # W.sort()
-for i in range(2,N,2):
-    pre_list[i-2] = pre_list[i-1] + abs(H[i+1] -H[i])
-    back_list[i-2] = back_list[i-1] + abs(H[N-i] - H[N-i-1])
+for i in range(N//2):
+    pre_list[i] = pre_list[i-1] + abs(H[2*i] -H[2*i+1]) if i!=0 else abs(H[0]-H[1])
+    back_list[i] = back_list[i-1] + abs(H[N-1-2*i] - H[N-2-2*i]) if i != 0 else abs(H[N-1] - H[N-2])
 ans = float('inf')
 print(pre_list)
 print(back_list)
 for i in range(M):
     w = W[i]
     ind = bisect.bisect_left(H,w)
-    if ind % 2 ==1:continue
-    print(ind)
-    tmp_sum = pre_list[ind//2-1]+back_list[(N//2)-1-(ind//2)]
-    tmp_sum+= abs(w-H[ind])
+    if ind == N:
+        ind-=1
+    if ind % 2 == 0:
+        tmp_sum = pre_list[ind//2-1]+back_list[(N//2)-1-(ind//2)]
+        tmp_sum+= abs(w-H[ind])
+    else:
+        ind1,ind2 = ind-1,ind+1
+        tmp_sum = min(pre_list[ind1//2-1]+back_list[(N//2)-1-(ind1//2)],pre_list[ind2//2-1]+back_list[(N//2)-1-(ind2//2)])
+
     ans = min(ans, tmp_sum)
 print(ans)

7dfbe49bd8a39ce67fb901ba81c0e1fddccae77e
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-03-03 01:06:17 +0900

    4

--- atcoder/contested/175/d.py
+++ atcoder/contested/175/d.py
@@ -1,23 +1,25 @@
 N,K=list(map(int,input().split()))
 P=list(map(lambda x : int(x)-1,input().split()))
 C=list(map(int,input().split()))
-ans = 0
+ans = float('-inf')
 for i in range(N):
     round_sum = 0
     round_points=[]
     x = i
     while True:
-        if P[x]==i:break
-        round_points.append(C[x])
         x=P[x]
+        round_points.append(C[x])
         round_sum+=C[x]
+        if x==i:break
+
     tmp = 0
     size = len(round_points)
     for j in range(size):
         tmp += round_points[j]
         now = tmp
+        if j+1 > K:break
         if round_sum>0:
-            now += round_sum*(size//K)
+            now += round_sum*((K-j-1)//size)
         ans = max(ans, now)
 
 print(ans)

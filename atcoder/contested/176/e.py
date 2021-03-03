5a1250b93985f170c26f5a717917dc2b6b7b278c
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-03-04 00:38:04 +0900

    1

--- atcoder/contested/176/e.py
+++ atcoder/contested/176/e.py
@@ -2,20 +2,21 @@ import sys
 input = sys.stdin.readline
 H,W,M=list(map(int,input().split()))
 grid = [["."] * W for _ in range(H)]
+h_cnt=[0]*H
+w_cnt=[0]*W
 for _ in range(M):
     h,w=list(map(lambda x : int(x)-1,input().split()))
     grid[h][w]='#'
-h_sum = [[0]*(W+1) for _ in range(H+1)]
-w_sum = [[0]*(W+1) for _ in range(H+1)]
-sum_list = [[0]*W for _ in range(H)]
+    h_cnt[h]+=1
+    w_cnt[w]+=1
+h_max=max(h_cnt)
+h_list = list(filter(lambda x : h_cnt[x]==h_max, range(H)))
+w_max=max(w_cnt)
+w_list = list(filter(lambda x : w_cnt[x]==w_max, range(W)))
 ans = 0
-for h in range(H):
-    for w in range(W):
-        tmp_sum = h_sum[h-1][w]+w_sum[h][w-1]
-        if grid[h][w]=='#':
-            tmp_sum+=1
-            h_sum[h][w]=h_sum[h-1][w]+1
-            w_sum[h][w]=w_sum[h][w-1]+1
-        sum_list[h][w] = tmp_sum
-        ans = max(ans, tmp_sum)
+for h in h_list:
+    for w in w_list:
+        tmp_cnt=h_max+w_max
+        if grid[h][w]=='#':tmp_cnt-=1
+        ans=max(ans, tmp_cnt)
 print(ans)

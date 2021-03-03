e2bc5334e7ba48b233793d992b5dd8fd68d78ae7
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-03-04 00:56:44 +0900

    AC

--- atcoder/contested/176/e.py
+++ atcoder/contested/176/e.py
@@ -1,12 +1,14 @@
 import sys
 input = sys.stdin.readline
+from collections import defaultdict
 H,W,M=list(map(int,input().split()))
-grid = [["."] * W for _ in range(H)]
+# grid = [["."] * W for _ in range(H)]
+dict = defaultdict(bool)
 h_cnt=[0]*H
 w_cnt=[0]*W
 for _ in range(M):
     h,w=list(map(lambda x : int(x)-1,input().split()))
-    grid[h][w]='#'
+    dict[str(h)+","+str(w)]=True
     h_cnt[h]+=1
     w_cnt[w]+=1
 h_max=max(h_cnt)
@@ -16,7 +18,7 @@ w_list = list(filter(lambda x : w_cnt[x]==w_max, range(W)))
 ans = h_max+w_max-1
 for h in h_list:
     for w in w_list:
-        if grid[h][w] != '#':
+        if not dict[str(h)+","+str(w)]:
             ans += 1
             print(ans)
             exit()

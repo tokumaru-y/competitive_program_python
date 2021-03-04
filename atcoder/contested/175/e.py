403db42393d98c4eff6074446aa84be8b6ae5e3f
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-03-05 00:33:57 +0900

    tmp

--- atcoder/contested/175/e.py
+++ atcoder/contested/175/e.py
@@ -1,3 +1,5 @@
+import sys
+input = sys.stdin.readline
 R,C,K=list(map(int,input().split()))
 items = [[0] * C for _ in range(R)]
 dp = [[[0,0,0,0] for _ in range(C+1)] for _ in range(R+1)]
@@ -11,6 +13,6 @@ for i in range(1,R+1):
             dp[i][j][0]=max(dp[i-1][j][m],dp[i][j][0])
             dp[i][j][1]=max(dp[i-1][j][m]+items[i-1][j-1], dp[i][j][1])
             dp[i][j][m]=max(dp[i][j-1][m], dp[i][j][m])
-            dp[i][j][m]=max(dp[i][j-1][m], dp[i][j-1][m-1]+items[i-1][j-1])
-
+            pre = dp[i][j-1][m-1] if m > 0 else 0
+            dp[i][j][m]=max(dp[i][j][m], pre+items[i-1][j-1])
 print(max(dp[R][C]))

30383b425f496db3e005c0951d339109f96f1a63
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-03-05 00:50:13 +0900

    AC

--- atcoder/contested/175/e.py
+++ atcoder/contested/175/e.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 R,C,K=list(map(int,input().split()))
 items = [[0] * C for _ in range(R)]
-dp = [[[0,0,0,0] for _ in range(C+1)] for _ in range(R+1)]
+dp = [[[0]*(C+1) for _ in range(R+1)] for _ in range(4)]
 for _ in range(K):
     r,c,v=list(map(int,input().split()))
     r,c=r-1,c-1
@@ -10,9 +10,8 @@ for _ in range(K):
 for i in range(1,R+1):
     for j in range(1,C+1):
         for m in range(1,4):
-            dp[i][j][0]=max(dp[i-1][j][m],dp[i][j][0])
-            dp[i][j][1]=max(dp[i-1][j][m]+items[i-1][j-1], dp[i][j][1])
-            dp[i][j][m]=max(dp[i][j-1][m], dp[i][j][m])
-            pre = dp[i][j-1][m-1] if m > 0 else 0
-            dp[i][j][m]=max(dp[i][j][m], pre+items[i-1][j-1])
-print(max(dp[R][C]))
+            dp[0][i][j]=max(dp[m][i-1][j],dp[0][i][j])
+            dp[1][i][j]=max(dp[m][i-1][j]+items[i-1][j-1], dp[1][i][j])
+            dp[m][i][j]=max(dp[m][i][j-1], dp[m][i][j])
+            dp[m][i][j]=max(dp[m][i][j], dp[m-1][i][j-1]+items[i-1][j-1])
+print(max(dp[0][R][C],dp[1][R][C],dp[2][R][C],dp[3][R][C]))

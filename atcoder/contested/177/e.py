c01001def9356d2050d486b389480652e8613a33
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-03-06 23:51:16 +0900

    AC

--- atcoder/contested/177/e.py
+++ atcoder/contested/177/e.py
@@ -1,11 +1,30 @@
 N=int(input())
 A=list(map(int,input().split()))
-def gcd(x,y):
+m_num=max(A)
+prime = [True]*(m_num+1)
+num_list = [0]*(m_num+1)
+def GCD(x,y):
     if y==0:
         return x
-    return gcd(y, x%y)
-
-coprime = True
-num_list = [0]*N
+    return GCD(y,x%y)
 for a in A:
     num_list[a]+=1
+coprime = True
+for i in range(2,m_num+1):
+    if not prime[i]:continue
+    tmp_sum = 0
+    for j in range(i,m_num+1,i):
+        prime[j]=False
+        tmp_sum+=num_list[j]
+    if tmp_sum > 1 :
+        coprime=False
+if coprime:
+    print("pairwise coprime")
+    exit()
+g = 0
+for a in A:
+    g = GCD(g,a)
+if g==1:
+    print("setwise coprime")
+else:
+    print("not coprime")

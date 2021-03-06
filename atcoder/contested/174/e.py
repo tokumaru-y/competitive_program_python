9a16daba6b8df52453fa898206378e68dead02de
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-03-06 17:08:51 +0900

    5

--- atcoder/contested/174/e.py
+++ atcoder/contested/174/e.py
@@ -3,15 +3,16 @@ A=list(map(int,input().split()))
 left = 0
 right=10**9
 ans = float('inf')
-while right - left > 1:
-    tmp_sum = 0
-    middle = (right+left)//2
+def search(length):
+    cnt = 0
     for a in A:
-        tmp_sum+=(a//middle) -1
-    if tmp_sum >K:
+        cnt += (a-1)//length
+    return cnt <= K
+
+while right - left > 1:
+    middle = (right+left)//2
+    if search(middle):
         right = middle
     else:
-        left = middle 
-    if tmp_sum > 0:
-        ans = min(ans,tmp_sum)
-print(ans)
+        left = middle
+print(right)

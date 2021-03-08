dc586c27a4d55262afbd6e27e781d884af40efbb
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-03-08 22:04:07 +0900

    4

--- atcoder/contested/173/d.py
+++ atcoder/contested/173/d.py
@@ -3,15 +3,8 @@ A=list(map(int,input().split()))
 A.sort(reverse=True)
 min_num=A[-1]
 ans = A[0]
-cnt = {}
-for a in A[1:]:
-    if a in cnt:
-        cnt[a]+=1
-    else:
-        cnt[a]=1
-for k,v in cnt.items():
-    if k==min_num:
-        ans+=k*(v-1)
-    else:
-        ans+=k*v
+index = 1
+for i in range(2,N):
+    ans+=A[index]
+    if i%2==1:index+=1
 print(ans)

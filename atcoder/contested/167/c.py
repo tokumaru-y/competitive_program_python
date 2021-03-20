3c845841d17303b78012807523aebecd9174b2cf
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-03-20 16:07:21 +0900

    3

---
+++ atcoder/contested/167/c.py
@@ -0,0 +1,15 @@
+N,M,X=list(map(int,input().split()))
+ans = float('inf')
+money = [list(map(int,input().split())) for _ in range(N)]
+
+for b in range(1<<N):
+    tmp_sum = [0] * M
+    tmp_m = 0
+    for i in range(N):
+        if b & 1<<i:
+            tmp_m += money[i][0]
+            for index, m in enumerate(money[i][1:]):
+                tmp_sum[index-1]+=m
+    if min(tmp_sum) >= X:
+        ans = min(ans, tmp_m)
+print(ans)

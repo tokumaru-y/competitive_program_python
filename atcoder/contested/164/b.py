b82b750f15299a775666cd0de3b9afee15cb56be
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-03-24 00:57:45 +0900

    2

---
+++ atcoder/contested/164/b.py
@@ -0,0 +1,12 @@
+a,b,c,d = list(map(int,input().split()))
+for i in range(105):
+    if i%2==0:
+        c -= b
+    else:
+        a -=d
+    if a<=0:
+        print("No")
+        exit()
+    elif c<=0:
+        print("Yes")
+        exit()

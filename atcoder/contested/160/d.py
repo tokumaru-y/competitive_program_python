c3a91110f268d12dd74df2c26a7e8af88693f3bb
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-03-28 20:31:45 +0900

    4

--- atcoder/contested/160/d.py
+++ atcoder/contested/160/d.py
@@ -1,31 +1,15 @@
 from collections import deque
 N,X,Y = list(map(int, input().split()))
-X,Y = X-1, Y-1
-node = [[] for _ in range(N)]
-for i in range(N-1):
-    node[i].append(i+1)
-    node[i+1].append(i)
-    if i==X:
-        node[X].append(Y)
-        node[Y].append(X)
-leng = []
-def BFS(x):
-    q = deque([x])
-    tmp = [0]*N
-    passed = [False]*N
-    passed[x] = True
-    while len(q)>0:
-        next = q.popleft()
-        for n in node[next]:
-            if passed[n]:continue
-            tmp[n] = tmp[next]+1
-            passed[n] = True
-            q.append(n)
-    return tmp
-
-cost = [BFS(i) for i in range(N)]
-for k in range(1, N):
-    ans = 0
-    for c in cost:
-        ans += c.count(k)
-    print(ans//2)
+cnt = {}
+for i in range(1,N):
+    for j in range(i+1,N+1):
+        tmp = min(j-i,abs(X-i)+1+abs(Y-j), abs(Y-i)+1+abs(X-j))
+        if tmp in cnt:
+            cnt[tmp]+=1
+        else:
+            cnt[tmp]=1
+for k in range(1,N):
+    if k in cnt:
+        print(cnt[k])
+    else:
+        print(0)

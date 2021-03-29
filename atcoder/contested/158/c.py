a9c90b12b1bc5a92894d13ee0c1e64b0a5d6e41c
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-03-30 00:21:09 +0900

    3

---
+++ atcoder/contested/158/c.py
@@ -0,0 +1,9 @@
+import math
+A,B=list(map(int, input().split()))
+for i in range(1,101):
+    a = math.floor(i*8/100)
+    b = math.floor(i*10/100)
+    if a==A and b==B:
+        print(i)
+        exit()
+print(-1)

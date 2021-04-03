f8f3a1d0c3995cc72fd8b4505e76bdf09c93c771
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-04-03 23:12:40 +0900

    1

--- atcoder/contested/156/b.py
+++ atcoder/contested/156/b.py
@@ -1,6 +1,6 @@
 N,K=list(map(int,input().split()))
-def gcd(x,y,cnt):
-    cnt+=1
-    if y==0:return cnt
-    return gcd(y,x%y,cnt)
-print(gcd(N,K,0))
+ans = []
+while N != 0 :
+    ans.append(N%K)
+    N//=K
+print(len(ans))

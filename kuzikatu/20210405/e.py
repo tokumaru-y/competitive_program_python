4f29a86f210b83268616a7f064648e81c1d36562
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-04-06 00:39:36 +0900

    add link

--- kuzikatu/20210405/e.py
+++ kuzikatu/20210405/e.py
@@ -9,20 +9,4 @@ for a in A:
         minus.append(a)
 plus.sort()
 minus.sort()
-if len(minus) == 0:
-    tmp_sum = 0
-    for i in range(1,len(plus)-1):
-        tmp_sum += plus[i-1] - plus[i]
-    ans = plus[-1] - tmp_sum
-elif len(plus) == 0:
-    tmp_sum = 0
-    for i in reversed(range(1,len(minus)-1)):
-        tmp_sum += minus[i] - minus[i-1]
-    ans = tmp_sum - minus[0]
-else:
-    tmp_sum = 0
-    sum_list = minus[1:] + plus[:len(plus)]
-    for i in range(1,len(sum_list)):
-        tmp_sum += sum_list[i] - sum_list[i-1]
-    ans = (plus[-1] - minus[0]) - tmp_sum
-print(ans)
+# https://atcoder.jp/contests/diverta2019-2/tasks/diverta2019_2_c

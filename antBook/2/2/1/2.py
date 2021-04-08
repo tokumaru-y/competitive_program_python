ebefea53df2baaf9a2cfa3e1cb2934d248fb3db4
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-04-08 21:43:02 +0900

    2

--- antBook/2/2/1/2.py
+++ antBook/2/2/1/2.py
@@ -1,17 +1,9 @@
 N,M=list(map(int, input().split()))
 coins = list(map(int, input().split()))
-coins.sort()
-coins = coins[1:]
-ans = float('inf')
-for bit in range(1<<(M-1)):
-    tmp_cnt = 0
-    left = N
-    for j in reversed(range(M-1)):
-        if bit & 1<<j:
-            cnt = left//coins[j]
-            left -= coins[j] * cnt
-            tmp_cnt += cnt
-    tmp_cnt += left
-    ans=min(ans, tmp_cnt)
-print(coins, len(coins))
-print(ans)
+dp = [float('inf')] * 50001
+dp[0] = 0
+for i in range(N+1):
+    for c in coins:
+        if i - c >= 0:
+            dp[i] = min(dp[i-c] + 1, dp[i])
+print(dp[N])

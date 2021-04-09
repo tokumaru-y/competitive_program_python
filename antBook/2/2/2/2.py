793128c7514cdccfce579431dd4e01bffce17308
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-04-09 23:24:45 +0900

    1

--- antBook/2/2/2/2.py
+++ antBook/2/2/2/2.py
@@ -1,13 +1,13 @@
 N,M=list(map(int, input().split()))
 AB=[]
 for _ in range(M):
-    a,b=list(map(lambda x :int(x)-1, input().split()))
+    a,b=list(map(lambda x :int(x), input().split()))
     AB.append([b,a])
 AB.sort()
 ans = 0
 limit = 0
 for b,a in AB:
-    if a > limit:
+    if a > limit or limit >= b:
         limit = b-1
         ans +=1
 print(ans)

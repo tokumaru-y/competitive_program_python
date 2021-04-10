2bed7dc3ff4be62472f252476550b6cbd958b825
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-04-10 22:39:18 +0900

    5

--- kuzikatu/20210410/e.py
+++ kuzikatu/20210410/e.py
@@ -2,28 +2,32 @@ from collections import deque
 import copy
 H,W=list(map(int, input().split()))
 grid = [input().rstrip() for _ in range(H)]
-pre = [[False] * W for _ in range(H)]
+passed = [[False] * W for _ in range(H)]
 ans = 0
-hi = [0,1,0,-1]
-wi = [1,0,-1,0]
+hi = [1,0,-1,0]
+wi = [0,1,0,-1]
 for i in range(H):
     for j in range(W):
-        passed = copy.deepcopy(pre)
+        if grid[i][j] == '.':continue
+        if passed[i][j]:continue
         passed[i][j] = True
-        origin_black = True if grid[i][j] == '#' else False
-        d = deque([[i,j,origin_black]])
+        d = deque([[i,j,True]])
+        p_list = [[i,j]]
+        cnt = 0
         while len(d) > 0:
-            nh,nw,is_black = d.popleft()
+            nh, nw, is_black = d.popleft()
             for k in range(4):
-                h = nh+hi[k]
-                w = nw+wi[k]
+                h,w = nh+hi[k], nw+wi[k]
                 if 0<=h<=H-1 and 0<=w<=W-1:
                     if passed[h][w]:continue
-                    if is_black and grid[h][w] == '#':continue
-                    if not is_black and grid[h][w] == '.':continue
-                    if origin_black and grid[h][w] == '.':ans+=1
-                    if not origin_black and grid[h][w] == '#':ans+=1
+                    if is_black:
+                        if grid[h][w] =='#':continue
+                        else:cnt+=1
+                    else:
+                        if grid[h][w] == '#':
+                            p_list.append([h,w])
+                        else:continue
                     passed[h][w] = True
-                    d.append([h,w,grid[h][w]=='#'])
-        pre[i][j] = True
+                    d.append([h,w,grid[h][w] == '#'])
+        ans+=cnt*len(p_list)
 print(ans)

67eeae69e7c83cf51dab2ea67a11e71485b0dd00
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-04-11 22:54:24 +0900

    3

--- atcoder/contested/198/c.py
+++ atcoder/contested/198/c.py
@@ -1,10 +1,9 @@
 import math
 R,X,Y=list(map(int, input().split())) 
 d = math.sqrt(X**2+Y**2)
-d2 = math.isqrt(X**2+Y**2)
-f = d2
-if math.ceil(d) > d2:
-    f = math.ceil(d)
-ans = (f//R)
-if not f%R==0: ans += 1
-print(int(ans))
+if R == d:
+    print(1)
+elif d<=2*R:
+    print(2)
+else:
+    print(math.ceil(d/R))

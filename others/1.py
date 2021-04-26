0161fc431a4a363e8af854603ba47965eadcb417
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2021-04-26 23:50:57 +0900

    tmp

---
+++ others/1.py
@@ -0,0 +1,10 @@
+# N=int(input())
+# graph = [] * N
+# for _ in range(N-1):
+#     u,v,w = list(map(int, input().split()))
+#     u,v = u-1, v-1
+#     graph[u].append([v,w])
+#     graph[v].append([u,w])
+# def dfs()
+# #https://qiita.com/drken/items/a803d4fc4a727e02f7ba の二部グラフの例題
+# # https://atcoder.jp/contests/abc126/tasks/abc126_d

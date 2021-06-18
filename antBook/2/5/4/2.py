ab8091a8267f357f20b1a6bcdc43ff8daefa9968
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-06-19 00:06:24 +0900

    5

--- antBook/2/5/4/2.py
+++ antBook/2/5/4/2.py
@@ -4,23 +4,29 @@ for _ in range(M):
     a,b,c = list(map(int, input().split()))
     a-=1
     b-=1
-    graph[a].append([b,c])
-dist = [float('-inf')] * N
-dist[0]=0
-is_inf_cycle = False
+    graph[a].append([b,-c])
+dist = [float('inf')] * N
+dist[0] = 0
 for count in range(N):
     is_update = False
     for v in range(N):
-        if dist[v] == float('-inf'):continue
-        for nv, cost in graph[v]:
-            if dist[v] + cost > dist[nv]:
-                dist[nv] = dist[v] + cost
+        if dist[v] == float('inf'):continue
+        for nv, point in graph[v]:
+            if dist[nv] > dist[v] + point:
+                dist[nv] = dist[v] + point
                 is_update = True
     if not is_update:break
-    if count == N-1:
-        is_inf_cycle = True
-        break
-if is_inf_cycle:
+negative_flag = [False] * N
+for count in range(N):
+    for v in range(N):
+        if dist[v] == float('inf'):continue
+        for nv, point in graph[v]:
+            if dist[nv] > dist[v] + point:
+                dist[nv] = dist[v] + point
+                negative_flag[nv] = True
+            if negative_flag[v] == True:
+                negative_flag[nv] = True
+if negative_flag[N-1]:
     print("inf")
 else:
-    print(dist[N-1])
+    print(-dist[N-1])

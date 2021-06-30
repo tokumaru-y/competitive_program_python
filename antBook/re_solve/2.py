8ac56d20bf2bbec209d4026654878fe34b00082c
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-07-01 00:39:54 +0900

    1

--- antBook/re_solve/2.py
+++ antBook/re_solve/2.py
@@ -1,33 +1,35 @@
 from heapq import heapify, heappop, heappush
 N=int(input())
 A=list(map(int, input().split()))
-S = [0] * (N+1)
-T = [0] * (N+1)
-S[0] = sum(A[:N])
-T[N] = sum(A[2*N:])
-hs = A[:N]
-ht = [ -1*a for a in A[2*N:]]
-heapify(hs)
-heapify(ht)
-
-for i in range(N, 2*N):
-    s = hs[0]
-    if s < A[i]:
-        S[i+1-N] = S[i-N] + A[i] + s
-        heappop(hs)
-        heappush(hs, A[i])
+S = [0] * (N*2+1)
+T = [0] * (N*2+1)
+s = []
+t = []
+for i in range(N):
+    S[i+1] = S[i] + A[i]
+    s.append(A[i])
+for index, i in enumerate(reversed(range(N*2,N*3))):
+    T[index+1] = T[index] + A[i]
+    t.append(-A[i])
+heapify(s)
+heapify(t)
+for i in range(N, N*2):
+    top = s[0]
+    if top < A[i]:
+        S[i+1] = S[i] + A[i] - top
+        heappop(s)
+        heappush(s, A[i])
     else:
-        S[i+1-N] = S[i-N]
-
-for i in reversed(range(N, 2*N)):
-    t = -ht[0]
-    if t > A[i]:
-        T[i-N-1] = T[i-N] + t - A[i]
-        heappop(ht)
-        heappush(ht, -A[i])
+        S[i+1] = S[i]
+for index, i in enumerate(reversed(range(N, N*2))):
+    top = -t[0]
+    if top > A[i]:
+        T[index+N+1] = T[index+N] + A[i] - top
+        heappop(t)
+        heappush(t, -A[i])
     else:
-        T[i-N-1] = T[i-N]
+        T[index+N+1] = T[index+N]
 ans = float("-inf")
-for i in range(N+1):
-    ans = max(ans, S[i] - T[i])
+for i in range(N,N*2+1):
+    ans = max(ans, S[i] - T[N*3-i])
 print(ans)

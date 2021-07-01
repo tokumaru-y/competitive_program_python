fa3356041e841ed495fb4db0ddf6a678bdfab3ff
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-07-02 00:13:42 +0900

    solve 6

--- antBook/re_solve/6.py
+++ antBook/re_solve/6.py
@@ -2,7 +2,6 @@ class UF:
     def __init__(self, x):
         self.par = [i for i in range(x)]
         self.deepth = [1 for _ in range(x)]
-        self.list = [i for i in range(x)]
     
     def root(self, x):
         if x == self.par[x]: return x
@@ -23,7 +22,6 @@ class UF:
         if self.deepth[rx] > self.deepth[ry]: rx, ry = ry, rx
         self.deepth[ry] += self.deepth[rx]
         self.par[rx] = self.par[ry]
-        self.list[ry] += self.list[rx]
 
 N,K,L=list(map(int, input().split()))
 road_tree = UF(N)
@@ -38,7 +36,19 @@ for _ in range(L):
     a-=1
     b-=1
     train_tree.unite(a,b)
-ans = 0
+dict = {}
 for i in range(N):
-    r_list = road_tree.
-print(ans)
+    rt = train_tree.root(i)
+    rr = road_tree.root(i)
+    if dict.get(rt) is None:
+        dict[rt] = {}
+    if dict[rt].get(rr) is None:
+        dict[rt][rr] = 1
+    else: 
+        dict[rt][rr] += 1
+ans = []
+for i in range(N):
+    rt = train_tree.par[i]
+    rr = road_tree.par[i]
+    ans.append(dict[rt][rr])
+print(*ans)

00250df42fbcacc784b5890d5027206fe4fc7fac
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-07-01 23:30:22 +0900

    tmp

---
+++ antBook/re_solve/6.py
@@ -0,0 +1,44 @@
+class UF:
+    def __init__(self, x):
+        self.par = [i for i in range(x)]
+        self.deepth = [1 for _ in range(x)]
+        self.list = [i for i in range(x)]
+    
+    def root(self, x):
+        if x == self.par[x]: return x
+        self.par[x] = self.root(self.par[x])
+        return self.par[x]
+    
+    def same(self, x, y):
+        rx = self.root(x)
+        ry = self.root(y)
+        return rx == ry
+    
+    def unite(self, x, y):
+        """もし、すでに同じグループならFalseを返す.
+        """
+        rx = self.root(x)
+        ry = self.root(y)
+        if rx == ry:return False
+        if self.deepth[rx] > self.deepth[ry]: rx, ry = ry, rx
+        self.deepth[ry] += self.deepth[rx]
+        self.par[rx] = self.par[ry]
+        self.list[ry] += self.list[rx]
+
+N,K,L=list(map(int, input().split()))
+road_tree = UF(N)
+train_tree = UF(N)
+for _ in range(K):
+    a,b = list(map(int, input().split()))
+    a-=1
+    b-=1
+    road_tree.unite(a,b)
+for _ in range(L):
+    a,b = list(map(int, input().split()))
+    a-=1
+    b-=1
+    train_tree.unite(a,b)
+ans = 0
+for i in range(N):
+    r_list = road_tree.
+print(ans)

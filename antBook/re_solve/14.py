6aa49664c7ef3b0dad6026b20dfcf042412c55dd
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-07-08 23:36:57 +0900

    tmp

--- antBook/re_solve/14.py
+++ antBook/re_solve/14.py
@@ -1,7 +1,7 @@
 from heapq import heapify, heappop, heappush
 N,M,S,T=list(map(int, input().split()))
-yen = [[float("inf")] * N for _ in range(N)]
-snuuke = [[float("inf")] * N for _ in range(N)]
+yen = [float("inf")] * N
+snuuke = [float("inf")] * N
 graph_yen = [[] for _ in range(N)]
 graph_snuuke = [[] for _ in range(N)]
 for _ in range(M):
@@ -14,41 +14,35 @@ for _ in range(M):
     graph_snuuke[v].append([u,b])
 S-=1
 T-=1
-yen[S][S] = 0
-snuuke[T][T] = 0
+yen[S] = 0
+snuuke[T] = 0
 
 h = [[0,S]]
 passed = [False] * N
 passed[S] = True
 heapify(h)
-cnt = 1
 while len(h) > 0:
     cost, nx = heappop(h)
     for v, c in graph_yen[nx]:
         if passed[v]:continue
         passed[v] = True
-        cnt+=1
-        yen[S][v] = cost + c
-        if cnt == N:break
+        yen[v]=min(yen[v], cost+c)
         heappush(h, [cost+c, v])
 h = [[0, T]]
 passed = [False] * N
 passed[T]=True
 heapify(h)
-cnt =1
 while len(h) > 0:
     cost, nx = heappop(h)
     for v, c in graph_snuuke[nx]:
         if passed[v]:continue
         passed[v] = True
-        snuuke[v][T] = cost + c
-        cnt+=1
-        if cnt == N:break
+        snuuke[v]=min(snuuke[v], cost+c)
         heappush(h, [cost+c, v])
 
 ans = float("inf")
 ll = []
 for i in reversed(range(N)):
-    ans = min(ans, yen[S][i] + snuuke[i][T])
+    ans = min(ans, yen[i] + snuuke[i])
     ll.append(10**15 - ans)
 print(*list(reversed(ll)), sep="\n")

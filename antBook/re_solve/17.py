6a391a15c4c922c0c5780f9610127c10d171783d
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-07-10 00:40:35 +0900

    solve17

--- antBook/re_solve/17.py
+++ antBook/re_solve/17.py
@@ -1,5 +1,6 @@
 N,M=list(map(int, input().split()))
-graph = [[float("inf")] * 7 for _ in range(N)]
+fgraph = [[float("inf")] * 4 for _ in range(N)]
+sgraph = [[float("inf")] * 7 for _ in range(N)]
 roads = [[] for _ in range(N)]
 for _ in range(M):
     a,b,c=list(map(int, input().split()))
@@ -7,20 +8,36 @@ for _ in range(M):
     roads[b].append([a,c])
 from heapq import heapify,heappop,heappush
 h=[[0,0]]
-graph[0][0]=0
 heapify(h)
-while len(h)>0:
+ans = [float("inf"), float("inf")]
+while len(h) > 0:
     cost, nx = heappop(h)
-    for x, c in roads[nx]:
-        next_i = (cost+c)%7
+    for x,c in roads[nx]:
         total_cost = cost+c
-        if graph[x][next_i] <= total_cost:continue
-        graph[x][next_i] = total_cost
+        ni = total_cost%4
+        if fgraph[x][ni] <= total_cost:continue
+        fgraph[x][ni]=total_cost
         if x == N-1:
-            if (total_cost % 4 ==0 or total_cost % 7 == 0):
-                print(total_cost)
-                exit()
-            else:
-                continue
+            if ni == 0:
+                ans[0]=total_cost
+                h=[]
+                break
+            else:continue
         heappush(h, [total_cost, x])
-        
+h=[[0,0]]
+while len(h) > 0:
+    cost, nx = heappop(h)
+    for x,c in roads[nx]:
+        total_cost = cost+c
+        ni = total_cost%7
+        if sgraph[x][ni] <= total_cost:continue
+        sgraph[x][ni]=total_cost
+        if x == N-1:
+            if ni == 0:
+                ans[1]=total_cost
+                h=[]
+                break
+            else:continue
+        heappush(h, [total_cost, x])
+
+print(min(ans))

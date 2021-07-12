a8783acc802108b2c0b1f8b489138b350761fd5c
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-07-12 22:37:27 +0900

    solve4

---
+++ antBook/first_problem/4.py
@@ -0,0 +1,84 @@
+class UF:
+    """Union Find
+    UF(x):
+    self.par:　各頂点の親を表す
+    self.deepth: 頂点の深さ表す
+    """
+    def __init__(self, x):
+        """[summary]
+
+        Args:
+            x (int): 頂点数
+        """
+        self.par = [i for i in range(x)]
+        self.deepth = [1 for _ in range(x)]
+    
+    def root(self, x):
+        """[summary]
+
+        Args:
+            x ([int]): 探索対象の頂点
+
+        Returns:
+            int: 頂点の親頂点
+        """
+        if x == self.par[x]: return x
+        self.par[x] = self.root(self.par[x])
+        return self.par[x]
+    
+    def same(self, x, y):
+        """[summary]
+
+        Args:
+            x ([int]): 頂点
+            y ([int]): 頂点
+
+        Returns:
+            boolean: x,yが同じグループに属しているか
+        """
+        rx = self.root(x)
+        ry = self.root(y)
+        return rx == ry
+    
+    def unite(self, x, y):
+        """[summary]
+
+        Args:
+            x ([int]): 頂点
+            y ([int]): 頂点
+
+        Returns:
+            boolean:
+            　True: 各頂点が違う親で結びつけられた
+            　False: 各頂点が同じ親だった
+        """
+        rx = self.root(x)
+        ry = self.root(y)
+        if rx == ry:return False
+        if self.deepth[rx] > self.deepth[ry]: rx, ry = ry, rx
+        self.deepth[ry] += self.deepth[rx]
+        self.par[rx] = self.par[ry]
+        return True
+from heapq import heappush,heappop,heapify
+while True:
+    N,M=list(map(int, input().split()))
+    if N==0 and M==0:
+        exit()
+    edges = []
+    for _ in range(M):
+        x,y,z=list(map(int, input().split()))
+        x-=1
+        y-=1
+        edges.append([z,x,y])
+    heapify(edges)
+    U=UF(N)
+    count = 0
+    while len(edges) > 0:
+        cost,x,y = heappop(edges)
+        if U.same(x,y):
+            continue
+        U.unite(x,y)
+        count+=1
+        if count == N//2:
+            print(cost)
+            break

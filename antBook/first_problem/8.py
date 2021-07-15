502e8cb8d8a63e5d5d5749e819fc47a8f3276396
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-07-16 00:31:29 +0900

    1`

--- antBook/first_problem/8.py
+++ antBook/first_problem/8.py
@@ -18,10 +18,9 @@ for k in range(N):
         for j in range(N):
             dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j])
 ans = float("inf")
-for i in Rs:
-    tmp=0
-    for j in Rs:
-        if i == j:continue
-        tmp=max(tmp,dp[i][j])
+import itertools
+for ll in itertools.permutations(Rs):
+    tmp = 0
+    for i in range(1,R):tmp+=dp[ll[i-1]][ll[i]]
     ans=min(ans,tmp)
 print(ans)

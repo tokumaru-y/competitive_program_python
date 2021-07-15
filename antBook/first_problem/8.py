acfe389ca58d1ca3401ee05bd8b3166f3e541ef8
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-07-16 00:25:09 +0900

    tmp

---
+++ antBook/first_problem/8.py
@@ -0,0 +1,27 @@
+N,M,R=list(map(int, input().split()))
+Rs=list(map(lambda x : int(x)-1, input().split()))
+graph = [[] for _ in range(N)]
+for _ in range(M):
+    a,b,c=list(map(int ,input().split()))
+    a-=1
+    b-=1
+    graph[a].append([b,c])
+    graph[b].append([a,c])
+dp = [[float("inf")] * N for _ in range(N)]
+for i in range(N):dp[i][i]=0
+for i in range(N):
+    for x,c in graph[i]:
+        dp[i][x] = min(dp[i][x],c)
+        dp[x][i] = min(dp[x][i],c)
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j])
+ans = float("inf")
+for i in Rs:
+    tmp=0
+    for j in Rs:
+        if i == j:continue
+        tmp=max(tmp,dp[i][j])
+    ans=min(ans,tmp)
+print(ans)

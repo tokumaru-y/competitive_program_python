a381d1bde3d2d7705d826c223b2ebee3a94e3700
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-07-26 16:14:38 +0900

    36

---
+++ antBook/re_solve/36.py
@@ -0,0 +1,28 @@
+MAX = 510000
+MOD=10**9+7
+fac=[0] * MAX
+finv=[0]* MAX
+inv=[0] * MAX
+# テーブルの初期化
+def COMinit():
+    fac[0] = 1
+    fac[1] = 1
+    finv[0] = 1
+    finv[1] = 1
+    inv[1] = 1
+    for i in range(2, MAX):
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD // i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+
+# 二項係数計算
+def COM(n, k):
+    if n < k: return 0
+    if n < 0 or k < 0: return 0
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD
+
+
+N=int(input())
+K=int(input())
+COMinit()
+print(COM(N+K-1, K))

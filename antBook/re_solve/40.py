03a26961ef1d505d7a2a3e2196bee19ba7aeb940
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-08-01 10:51:29 +0900

    40

--- antBook/re_solve/40.py
+++ antBook/re_solve/40.py
@@ -1,9 +1,15 @@
-# N,Z,W=list(map(int, input().split()))
-# A=list(map(int, input().split()))
-# # dp[i][j]:= 山札からi枚取り除いたときの絶対値(jはAさんかどうか)
-# dp = [[True, False] for _ in range(N+1)]
-# dp[0]=abs(Z-W)
-# def dfs(dp, time):
-#     pass
-# dfs(dp, 0)
-# print(dp[N])
+N,Z,W=list(map(int, input().split()))
+A=list(map(int, input().split()))
+dp = [[0] * 2 for _ in range(N)]
+for i in reversed(range(N)):
+    dp[i][0]=float("-inf")
+    X = A[i-1] if i >0 else W
+    dp[i][0]=max(dp[i][0], abs(X-A[N-1]))
+    for k in range(i+1,N):
+        dp[i][0]=max(dp[i][0], dp[k][1])
+    dp[i][1]=float('inf')
+    Y = A[i-1] if i >0 else Z
+    dp[i][1]=min(dp[i][1], abs(Y-A[N-1]))
+    for k in range(i+1,N):
+        dp[i][1]=min(dp[i][1], dp[k][0])
+print(dp[0][0])

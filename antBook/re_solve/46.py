f26176d03f64519887d007e371f91857a18f52ae
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-08-09 15:15:30 +0900

    46

--- antBook/re_solve/46.py
+++ antBook/re_solve/46.py
@@ -1,24 +1,21 @@
+from collections import deque
 import sys
 sys.setrecursionlimit(10**9)
 H,W=list(map(int, input().split()))
 N=int(input())
 dp = [[0] * (W+1) for _ in range(H+1)]
-grid = [[0] * (W+1) for _  in range(H+1)]
+grid = [[1] * (W+1) for _  in range(H+1)]
 hi = [0,1]
 wi = [1,0]
-def dfs(h,w):
-    for i in range(2):
-        nh = h-hi[i]
-        nw = w-wi[i]
-        if not(0<=nh<=H and 0<=nw<=W):continue
-        if grid[nh][nw] == 1:
-            dp[h][w]=0
-        else:
-            dp[h][w]+= dp[nh][nw]
-        dfs(nh,nw)
 for _ in range(N):
     a,b=list(map(int, input().split()))
-    grid[a][b]=1
+    grid[a][b]=0
 dp[1][1]=grid[1][1]
-dfs(H,W)
-print(dp[0][0])
+for i in range(H):
+    for j in range(W):
+        if i == 0 and j== 0:continue
+        if grid[i+1][j+1]==0:
+            dp[i+1][j+1]=0
+        else:
+            dp[i+1][j+1] = dp[i][j+1] + dp[i+1][j]
+print(dp[H][W])

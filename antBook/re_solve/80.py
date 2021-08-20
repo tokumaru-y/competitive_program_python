2969aa1a8a775a4639867d2dbcea0cac0026cbfb
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-08-20 22:34:03 +0900

    80

---
+++ antBook/re_solve/80.py
@@ -0,0 +1,76 @@
+class UF:
+    """Union Find
+    UF(x):
+    self.par:　各頂点の親を表す
+    self.deepth: 頂点の深さ表す
+    """
+    def __init__(self, x):
+        """[summary]
+
+        Args:
+            x (int): 頂点数
+        """
+        self.par = [i for i in range(x)]
+        self.deepth = [1 for _ in range(x)]
+    
+    def root(self, x):
+        """[summary]
+
+        Args:
+            x ([int]): 探索対象の頂点
+
+        Returns:
+            int: 頂点の親頂点
+        """
+        if x == self.par[x]: return x
+        self.par[x] = self.root(self.par[x])
+        return self.par[x]
+    
+    def same(self, x, y):
+        """[summary]
+
+        Args:
+            x ([int]): 頂点
+            y ([int]): 頂点
+
+        Returns:
+            boolean: x,yが同じグループに属しているか
+        """
+        rx = self.root(x)
+        ry = self.root(y)
+        return rx == ry
+    
+    def unite(self, x, y):
+        """[summary]
+
+        Args:
+            x ([int]): 頂点
+            y ([int]): 頂点
+
+        Returns:
+            boolean:
+            　True: 各頂点が違う親で結びつけられた
+            　False: 各頂点が同じ親だった
+        """
+        rx = self.root(x)
+        ry = self.root(y)
+        if rx == ry:return False
+        if self.deepth[rx] > self.deepth[ry]: rx, ry = ry, rx
+        self.deepth[ry] += self.deepth[rx]
+        self.par[rx] = self.par[ry]
+        return True
+N,Q=list(map(int, input().split()))
+u = UF(2*N)
+for _ in range(Q):
+    w,x,y,z = list(map(int, input().split()))
+    x-=1
+    y-=1
+    if w == 1:
+        if z%2 == 0:
+            u.unite(2*x, 2*y)
+            u.unite(2*x+1, 2*y+1)
+        else:
+            u.unite(2*x+1, 2*y)
+            u.unite(2*x, 2*y+1)
+    else:
+        print("YES" if u.same(2*x, 2*y) else "NO")

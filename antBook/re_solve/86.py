dfb0ef6b48bbf851e98e9664fcf4a4290b8c9f36
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-08-23 21:39:12 +0900

    tmp

--- antBook/re_solve/86.py
+++ antBook/re_solve/86.py
@@ -9,3 +9,27 @@ for _ in range(M):
     b=int(b)-1
     graph[a].append([b,int(c=='r')])
     graph[b].append([a,int(c=='r')])
+vis=[False]*N
+lenght=[-1]*N
+def dfs(t,c,len):
+    if lenght[t] < 0:
+        lenght[t] = len
+    elif abs(len - lenght[t])%2 == 1:
+        print("Yes")
+        exit()
+    for nt,nc in graph[t]:
+        if vis[nt]:continue
+        if nc == c:
+            vis[nt]=True
+            dfs(nt,abs(nc-1),len+1)
+    print(vis,lenght)
+for i in range(N):
+    for c in range(2):
+        vis = [False]*N
+        lenght=[-1]*N
+        vis[i]=True
+        dfs(i,c,0)
+        if all(vis):
+            print("Yes")
+            exit()
+print("No")

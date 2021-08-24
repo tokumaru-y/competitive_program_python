826148fd25d20c088306890873155b913fcff2fb
YukiInaba <tokumaru.program.yuuki@gmail.com>
2021-08-24 20:11:11 +0900

    91

---
+++ antBook/re_solve/91.py
@@ -0,0 +1,78 @@
+class UF:
+    """Union Find
+    UF(x):
+    self.par:　各頂点の親を表す
+    self.deepth: 頂点の深さ表す
+    """
+    def __init__(self, x):
+        """[summary]
+
+        Args:
+            x (int): 頂点数
+        """
+        self.par = [i for i in range(x)]
+        self.deepth = [1 for _ in range(x)]
+    
+    def root(self, x):
+        """[summary]
+
+        Args:
+            x ([int]): 探索対象の頂点
+
+        Returns:
+            int: 頂点の親頂点
+        """
+        if x == self.par[x]: return x
+        self.par[x] = self.root(self.par[x])
+        return self.par[x]
+    
+    def same(self, x, y):
+        """[summary]
+
+        Args:
+            x ([int]): 頂点
+            y ([int]): 頂点
+
+        Returns:
+            boolean: x,yが同じグループに属しているか
+        """
+        rx = self.root(x)
+        ry = self.root(y)
+        return rx == ry
+    
+    def unite(self, x, y):
+        """[summary]
+
+        Args:
+            x ([int]): 頂点
+            y ([int]): 頂点
+
+        Returns:
+            boolean:
+            　True: 各頂点が違う親で結びつけられた
+            　False: 各頂点が同じ親だった
+        """
+        rx = self.root(x)
+        ry = self.root(y)
+        if rx == ry:return False
+        if self.deepth[rx] > self.deepth[ry]: rx, ry = ry, rx
+        self.deepth[ry] += self.deepth[rx]
+        self.par[rx] = self.par[ry]
+        return True
+from heapq import heapify,heappush,heappop
+V,E=list(map(int, input().split()))
+graph = []
+for _ in range(E):
+    s,t,w=list(map(int, input().split()))
+    s-=1
+    t-=1
+    graph.append([w,s,t])
+heapify(graph)
+U = UF(V)
+ans=0
+while len(graph) > 0:
+    w,s,t = heappop(graph)
+    if U.same(s,t):continue
+    U.unite(s,t)
+    ans+=w
+print(ans)

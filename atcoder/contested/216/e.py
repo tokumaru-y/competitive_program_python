9e68fe932404afc945f558e067321e6a4b55f0e3
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-08-30 23:28:14 +0900

    e

--- atcoder/contested/216/e.py
+++ atcoder/contested/216/e.py
@@ -1,13 +1,18 @@
-from heapq import heapify, heappop,heappush
 N,K=list(map(int, input().split()))
 A=list(map(int,input().split()))
 A.sort(reverse=True)
+A += [0]
 ans = 0
-tmp_cnt = 0
 for i in range(N):
-    if i < N-1:
-        dif = A[i] - A[i+1]
-        
+    dif = A[i] - A[i+1]
+    cnt = dif*(i+1)
+    if cnt <= K:
+        K-= cnt
+        ans += (dif*(A[i] + A[i]-dif+1)//2)*(i+1)
     else:
-        if K>=A[i]:
-            #TASUDAKE
+        x = K // (i+1)
+        y = K % (i+1)
+        ans += x*(A[i] + A[i]-x+1)//2 * (i+1)
+        ans += (A[i] - x) * y
+        K=0
+print(ans)

b17a278aa45e76f3a3a4d69a33a91968ef2e9f61
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-09-02 19:36:52 +0900

    e

--- atcoder/per_contenst/214/e.py
+++ atcoder/per_contenst/214/e.py
@@ -4,41 +4,27 @@ grid = [list(input()) for _ in range(H)]
 h_dir = [0,1,0,-1]
 w_dir = [1,0,-1,0]
 hq = deque([[0,0,0]])
-passed = [[False]*W for _ in range(H)]
+cnt_grid = [[float("inf")]*W for _ in range(H)]
+cnt_grid[0][0]=0
+break_dir = [-1,0,1]
 while True:
     cnt,nh,nw = hq.popleft()
-    passed[nh][nw]=True
+    if nh==H-1 and nw==W-1:
+        print(cnt)
+        exit()
     for i in range(4):
         h,w = h_dir[i]+nh, w_dir[i]+nw
         if not(0<=h<=H-1 and 0<=w<=W-1):continue
-        if passed[h][w]:continue
-        if h == H-1 and w == W-1:
-            print(cnt)
-            exit()
-        if grid[h][w] == '.' and not passed[h][w]:
+        if cnt_grid[h][w] <= cnt:continue
+        if grid[h][w] == '.':
             hq.appendleft([cnt,h,w])
+            cnt_grid[h][w]=cnt
         else:
-            if i == 0:
-                for s in [-1,0,1]:
-                    if 0<=h+s<=H-1:
-                        if not passed[h+s][w]:hq.append([cnt+1,h+s,w])
-                        if w+1<=W-1 and not passed[h+s][w+1]:
-                            hq.append([cnt+1,h+s,w+1])
-            elif i == 1:
-                for s in [-1,0,1]:
-                    if 0<=w+s<=W-1:
-                        if not passed[h][w+s]:hq.append([cnt+1,h,w+s])
-                        if h+1<=H-1 and not passed[h+1][w+s]:
-                            hq.append([cnt+1,h+1,w+s])
-            elif i == 2:
-                for s in [-1,0,1]:
-                    if 0<=h+s<=H-1: 
-                        if not passed[h+s][w]:hq.append([cnt+1,h+s,w])
-                        if w-1>=0 and not passed[h+s][w-1]:
-                            hq.append([cnt+1,h+s,w-1])
-            elif i == 3:
-                for s in [-1,0,1]:
-                    if 0<=w+s<=W-1:
-                        if not passed[h][w+s]:hq.append([cnt+1,h,w+s])
-                        if h-1>=0 and not passed[h-1][w+s]:
-                            hq.append([cnt+1,h-1,w+s])
+            for a in range(3):
+                for b in range(3):
+                    th = h+break_dir[a]
+                    tw = w+break_dir[b]
+                    if not(0<=th<=H-1 and 0<=tw<=W-1):continue
+                    if cnt_grid[th][tw] <= cnt+1:continue
+                    hq.append([cnt+1,th,tw])
+                    cnt_grid[th][tw]=cnt+1

62697302580156ccf95da4bc54cbb6bda5b06e53
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-09-12 15:26:23 +0900

    C

--- atcoder/contested/218/c.py
+++ atcoder/contested/218/c.py
@@ -1,52 +1,45 @@
+
 N=int(input())
 S = [input() for _ in range(N)]
 T = [input() for _ in range(N)]
-hx=[0,1,0,-1]
-wx=[1,0,-1,0]
+
+
+def rootate(S):
+    return list(zip(*S[::-1]))
+
+def find_top(grid):
+    for i in range(N):
+        for j in range(N):
+            if grid[i][j] == "#":
+                return (i,j)
+
+
+def check(S,T):
+    sh, sw = find_top(S)
+    th, tw = find_top(T)
+    dif_h = th-sh
+    dif_w = tw-sw
+    for i in range(N):
+        for j in range(N):
+            nh = dif_h + i
+            nw = dif_w + j
+            if not (0<=nh<=N-1 and 0<=nw<=N-1):
+                if S[i][j] == "#":return False
+            else:
+                if S[i][j] != T[nh][nw]:return False
+    return True
+
+cnt_s = 0
+cnt_t = 0
 for i in range(N):
-    tmp = []
-    lll = []
-    if i == 1:
-        # 90
-        for h in range(N):
-            t=[]
-            for w in range(N):
-                if T[w][h] == "#":
-                    lll.append([h,w])
-                t.append(T[w][h])
-            tmp.append(t)
-    elif i == 2:
-        for h in range(N):
-            t=[]
-            for w in range(N):
-                if T[-h][w] == "#":
-                    lll.append([h,w])
-                t.append(T[-h][w])
-            tmp.append(t)
-    elif i == 3:
-        for h in range(N):
-            t=[]
-            for w in range(N):
-                if T[w][-h] == "#":
-                    lll.append([h,w])
-                t.append(T[w][-h])
-            tmp.append(t)
-    else:
-        tmp = T
-    for h in range(N):
-        for w in range(N):
-            if S[h][w] == "#":
-                for target in lll:
-                    dh = h-target[0]
-                    dw = w-target[1]
-                    for th,tw in lll:
-                        gh=th+dh
-                        gw=tw+dw
-                        if not(0<=gh<=N-1 and 0<=gw<=N-1):
-                            break
-                        if tmp[gh][gw] != "#":
-                            break
-                else:
-                    print("Yes")
-                    exit()
+    cnt_s += sum([1 if x=="#" else 0 for x in S[i]])
+    cnt_t += sum([1 if x=="#" else 0 for x in T[i]])
+if cnt_s != cnt_t:
+    print("No")
+    exit()
+for _ in range(4):
+    if check(S,T):
+        print("Yes")
+        exit()
+    S=rootate(S)
 print("No")

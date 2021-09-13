d09216b47fd14c80f9993a7559760b457349cb11
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-09-13 20:43:43 +0900

    add ext_gcd

--- own_lib/GCD.py
+++ own_lib/GCD.py
@@ -6,3 +6,10 @@ def gcd(n,m):
 def lcm(n,m):
     if n<m:n,m=m,n
     return (n*m) // gcd(n,m)
+
+def ext_gcd(a, b):
+    if b:
+        d, y, x = ext_gcd(b, a % b)
+        y -= (a // b)*x
+        return d, x, y
+    return a, 1, 0

6c68df6890c99931da072dbbfcf2937109c2b40f
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-09-21 23:08:02 +0900

    tmp

--- antBook/re_solve/134.py
+++ antBook/re_solve/134.py
@@ -1,33 +1,28 @@
 # https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_H&lang=jp
+import bisect
+
+
 N,W=list(map(int, input().split()))
 VW=[list(map(int, input().split())) for _ in range(N)]
-VW1 = VW[:N//2]
-VW2 = VW[N//2:]
-def calc(target_list):
-    n = len(target_list)
-    res = [[w,v] for v,w in target_list]
-    for i in range(n):
-        _v,_w = target_list[i]
-        for j in range(i+1, n):
-            v,w = target_list[j]
-            res.append([_w+w, _v+v])
-    return res
-
-accum1 = calc(VW1)
-accum2 = calc(VW2)
-accum2.append([0,0])
-accum2.append([float("inf"), float("inf")])
-accum2.sort()
+half = []
+for i in range(1<<(N//2)):
+    sv,sw = 0,0
+    for j in range(N//2):
+        if i & 1<<j:
+            sv += VW[j][0]
+            sw += VW[j][1]
+    if sw <= W:
+        half.append([sw,sv])
+half.sort()
 ans = 0
-from bisect import bisect_left
-print(accum1, VW1)
-for i in range(len(accum1)):
-    v,w = accum1[i]
-    target = W-w
-    if target < 0:continue
-    idx = bisect_left(accum2, target)
-    if accum2[idx][0] + w == W:
-        ans = max(ans, v + accum2[idx][1])
-    else:
-        ans = max(ans, v + accum2[idx-1][1])
+for i in range(1<<(N-N//2)):
+    sv,sw = 0,0
+    for j in range(N-N//2):
+        if i & 1<<j:
+            sv += VW[N//2+j][0]
+            sw += VW[N//2+j][1]
+    if sw<= W:
+        idx = bisect.bisect_left(half, [W-sw, float("inf")])
+        idx -=1
+        ans = max(ans, sv + half[idx][1])
 print(ans)

0d54a44773393356d77207b5ee1e60c5ea9fcd78
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-09-21 23:47:43 +0900

    134

--- antBook/re_solve/134.py
+++ antBook/re_solve/134.py
@@ -1,7 +1,5 @@
 # https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_H&lang=jp
 import bisect
-
-
 N,W=list(map(int, input().split()))
 VW=[list(map(int, input().split())) for _ in range(N)]
 half = []
@@ -11,9 +9,11 @@ for i in range(1<<(N//2)):
         if i & 1<<j:
             sv += VW[j][0]
             sw += VW[j][1]
-    if sw <= W:
-        half.append([sw,sv])
+    half.append([sw,sv])
 half.sort()
+for i in range(1, 1<<(N//2)):
+    if half[i-1][1] >= half[i][1]:
+        half[i] = half[i-1][:]
 ans = 0
 for i in range(1<<(N-N//2)):
     sv,sw = 0,0
@@ -22,7 +22,7 @@ for i in range(1<<(N-N//2)):
             sv += VW[N//2+j][0]
             sw += VW[N//2+j][1]
     if sw<= W:
-        idx = bisect.bisect_left(half, [W-sw, float("inf")])
-        idx -=1
+        idx = bisect.bisect_right(half, [W-sw, float("inf")])
+        idx -= 1
         ans = max(ans, sv + half[idx][1])
 print(ans)

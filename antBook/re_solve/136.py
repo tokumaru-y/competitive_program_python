0d6afae4e34fa9a51ebc4402103c7e3bbb93b3cd
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-09-23 14:43:06 +0900

    136

--- antBook/re_solve/136.py
+++ antBook/re_solve/136.py
@@ -1,5 +1,7 @@
 # https://atcoder.jp/contests/joi2008ho/tasks/joi2008ho_e
 W,H=list(map(int, input().split()))
+W*=2
+H*=2
 N=int(input())
 X1,X2,Y1,Y2=[],[],[],[]
 def convert(list1, list2, limit):
@@ -25,22 +27,17 @@ def format(list, format_list):
 X1, X2, Y1, Y2 = [],[],[],[]
 for _ in range(N):
     x1,y1,x2, y2 = list(map(int, input().split()))
-    X1.append(x1)
-    X2.append(x2)
-    Y1.append(y1)
-    Y2.append(y2)
+    X1.append(x1*2)
+    X2.append(x2*2)
+    Y1.append(y1*2)
+    Y2.append(y2*2)
 conv_x, size_x = convert(X1, X2, W)
 conv_y, size_y = convert(Y1, Y2, H)
 X1 = format(X1, conv_x)
 X2 = format(X2, conv_x)
 Y1 = format(Y1, conv_y)
 Y2 = format(Y2, conv_y)
-print("----")
-print(conv_x)
 grid = [[0] * (size_x) for _ in range(size_y)]
-print("------")
-for a,b in zip(X1,X2):
-    print(a,b)
 for i in range(N):
     for h in range(Y1[i], Y2[i]+1):
         for w in range(X1[i], X2[i]+1):
@@ -49,8 +46,6 @@ ans = 0
 dx = [0,1,-1,0]
 dy = [1,0,0,-1]
 from collections import deque
-for h in range(size_y):
-    print(grid[h])
 for h in range(size_y):
     for w in range(size_x):
         if grid[h][w]:continue

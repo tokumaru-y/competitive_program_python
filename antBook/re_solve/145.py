c784664513a9a5fb3d926cc9f86bcd4c951eceb5
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-09-25 18:25:57 +0900

    145

---
+++ antBook/re_solve/145.py
@@ -0,0 +1,67 @@
+# https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_A&lang=jp
+class SegTree:
+    # 引用元:https://qiita.com/takayg1/items/c811bd07c21923d7ec69
+    """
+    Segment Tree
+    """
+
+    def __init__(self, init_val, segfunc, ide_ele):
+        """
+        初期化
+
+        init_val: 配列の初期値
+        """
+        n = len(init_val)
+        self.segfunc = segfunc
+        self.ide_ele = ide_ele
+        self.num = 1 << (n - 1).bit_length()
+        self.tree = [ide_ele] * 2 * self.num
+        # 配列の値を葉にセット
+        for i in range(n):
+            self.tree[self.num + i] = init_val[i]
+        # 構築していく
+        for i in range(self.num - 1, 0, -1):
+            self.tree[i] = segfunc(self.tree[2 * i], self.tree[2 * i + 1])
+
+    def update(self, k, x):
+        """
+        k番目の値をxに更新
+
+        k: index(0-index)
+        x: update value
+        """
+        k += self.num
+        self.tree[k] = x
+        while k > 1:
+            self.tree[k >> 1] = self.segfunc(self.tree[k], self.tree[k ^ 1])
+            k >>= 1
+
+    def query(self, l, r):
+        """
+        [l, r)のsegfuncしたものを得る
+
+        l: index(0-index)
+        r: index(0-index)
+        """
+        res = self.ide_ele
+
+        l += self.num
+        r += self.num
+        while l < r:
+            if l & 1:
+                res = self.segfunc(res, self.tree[l])
+                l += 1
+            if r & 1:
+                res = self.segfunc(res, self.tree[r - 1])
+            l >>= 1
+            r >>= 1
+        return res
+
+N,Q=list(map(int, input().split()))
+seg = SegTree([2**31-1] * N, min, 2**31-1)
+for _ in range(Q):
+    c,x,y =list(map(int, input().split()))
+    if c == 0:
+        seg.update(x,y)
+    else:
+        print(seg.query(x,y+1))

d08829b418622debd078251a17cbe7615c4be13f
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-10-11 00:15:57 +0900

    167

--- antBook/re_solve/167.py
+++ antBook/re_solve/167.py
@@ -2,16 +2,21 @@
 N=int(input())
 XYZ=[list(map(int,input().split())) for _ in range(N)]
 dp = [[float("inf")] * (N) for _ in range(1<<N)]
-dp[0][0]=0
+dp[1][0]=0
 for bit in range(1<<N):
     for i in range(N):
         if bit & (1<<i):
-            for j in range(N):
-                if i == j or (bit & 1<<j): continue
+            for j in range(N):  
+                if i == j or not (bit & 1<<j): continue
                 x1,y1,z1=XYZ[i]
                 x2,y2,z2=XYZ[j]
-                print(bit,i,j)
-                dp[bit][i] = min(dp[bit][i], dp[bit^i][j] + abs(x1-x2) + abs(y1-y2) + max(0, z1-z2))
-for i in range(1<<N):
-    print(dp[i])
-print(min(dp[(1<<N) - 1 ]))
+                dp[bit][i] = min(dp[bit][i], dp[bit^(1<<i)][j] + abs(x1-x2) + abs(y1-y2) + max(0, z1-z2))
+ans = float("inf")
+for i in range(N):
+    tmp = dp[(1<<N)-1][i]
+    if i > 0:
+        x1,y1,z1=XYZ[0]
+        x2,y2,z2=XYZ[i]
+        tmp += abs(x1-x2) + abs(y1-y2) + max(0, z1-z2)
+    ans = min(ans, tmp)
+print(ans)

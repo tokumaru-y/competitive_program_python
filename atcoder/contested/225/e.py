862b042865bdf29fe6d48de02ea20270047d1cd4
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-10-31 09:28:59 +0900

    e

--- atcoder/contested/225/e.py
+++ atcoder/contested/225/e.py
@@ -1,16 +1,12 @@
-import math
+from math import inf
 N=int(input())
 XY=[list(map(int, input().split())) for _ in range(N)]
 dis = []
 for x,y in XY:
     sx,sy = x-1,y
     ex,ey = x,y-1
-    s = math.atan2(sy,sx)
-    e = math.atan2(ey,ex)
-    s = s + 2 * math.pi if s<0 else s
-    e = e + 2 * math.pi if e<0 else e
-    s = (s * 360) / (2*math.pi)
-    e = (e * 360) / (2*math.pi)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+    s = (10 ** 20 * (sy)//(sx)) if sx != 0 else inf
+    e = (10 ** 20 * (ey)//(ex)) if ey != 0 else -1
     dis.append([s,e])
 dis = sorted(dis, key= lambda x:(x[0],x[1]))
 ans = 0

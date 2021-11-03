a584731d83422767c65d6c504cac216a3a7c373b
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-11-03 11:46:38 +0900

    220

--- antBook/re_solve/400/220.py
+++ antBook/re_solve/400/220.py
@@ -5,17 +5,19 @@ A=[int(input()) for _ in range(N)]
 if sum(A) == K:
     print(1)
     exit()
-dp = [float("inf")] * (N+1)
-dp[0] = 0
-dp[1] = 1
+dp = [[float("inf")] * (N+1)  for _ in range(N+1)]
+dp[0][0] = 0
+dp[1][0] = 0
+dp[1][1] = 1
 sum_game = A[0]
 for i in range(1,N):
-    left_value = floor(((dp[i]*A[i]) / sum_game))
-    dp[i+1]=left_value + 1
-    sum_game+=A[i]
-tmp_cnt = 0
-for i in range(N):
-    tmp_cnt += dp[i+1]
-    if tmp_cnt > K:
+    for j in range(N):
+        if dp[i][j] == float("inf"):continue
+        pre_wins = floor((dp[i][j] * A[i]) / sum_game) + 1
+        dp[i+1][j+1] = min(dp[i+1][j+1], dp[i][j+1], pre_wins + dp[i][j])
+        dp[i+1][j] = min(dp[i+1][j], dp[i][j])
+    sum_game += A[i]
+for i in reversed(range(N+1)):
+    if dp[N][i]<=K:
         print(i)
         exit()

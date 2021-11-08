0b4f94a766957c334986200179785085c3c84f98
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-11-08 22:00:38 +0900

    227

---
+++ antBook/re_solve/400/227.py
@@ -0,0 +1,24 @@
+# https://atcoder.jp/contests/tdpc/tasks/tdpc_game
+A,B=list(map(int, input().split()))
+As=list(map(int, input().split()))
+Bs=list(map(int, input().split()))
+# dp[i][j]:=左の山からi枚,右の山からJ枚取り除かれている状態からスタートした場合、最終的な先手の最大値
+dp = [[0] * (B+1) for _ in range(A+1)]
+for i in reversed(range(A+1)):
+    for j in reversed(range(B+1)):
+        if i == A and j == B:continue
+        if (i+j)%2 == 0:
+            if i == A:
+                dp[i][j] = dp[i][j+1] + Bs[j]
+            elif j == B:
+                dp[i][j] = dp[i+1][j] + As[i]
+            else:
+                dp[i][j] = max(dp[i+1][j] + As[i], dp[i][j+1] + Bs[j])
+        else:
+            if i == A:
+                dp[i][j] = dp[i][j+1]
+            elif j == B:
+                dp[i][j] = dp[i+1][j]
+            else:
+                dp[i][j] = min(dp[i+1][j], dp[i][j+1])
+print(dp[0][0])

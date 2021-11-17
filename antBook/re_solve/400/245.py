d115426a1743b29e4e8d6856e3754a040a72ad3a
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-11-17 19:23:35 +0900

    245

---
+++ antBook/re_solve/400/245.py
@@ -0,0 +1,14 @@
+# https://atcoder.jp/contests/keyence2020/tasks/keyence2020_c
+N,K,S=list(map(int, input().split()))
+ans = []
+if N == K:
+    ans = [S for _ in range(N)]
+else:
+    if K == N-1:
+        ans = [1 if i%2==0 else S-1 for i in range(N)]
+    else:
+        ans = [1 if i%2==0 else S-1 for i in range(K+1)]
+        t = 10**9 if S < 10**9 else 2
+        for i in range(N-(K+1)):
+            ans.append(t)
+print(*ans, sep=" ")

2db5f83993bc8ba0819153fc5aacb813fd32b832
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-11-17 19:31:28 +0900

    245

--- antBook/re_solve/400/245.py
+++ antBook/re_solve/400/245.py
@@ -3,6 +3,10 @@ N,K,S=list(map(int, input().split()))
 ans = []
 if N == K:
     ans = [S for _ in range(N)]
+elif S == 1:
+    ans = [1 for _ in range(K)]
+    for _ in range(N-K):
+        ans.append(10**9)
 else:
     if K == N-1:
         ans = [1 if i%2==0 else S-1 for i in range(N)]
